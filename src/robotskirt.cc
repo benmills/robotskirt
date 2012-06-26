8dcf3970fd2655f95bbcfcde1ed3c99d5b5e3832
Xavier Mendez <jmendeth@gmail.com>
2012-06-26 19:30:33 +0200

    Organize, cleanup original code; add some utilities

--- src/robotskirt.cc
+++ src/robotskirt.cc
@@ -15,6 +15,11 @@ using namespace mkd;
 
 #define OUTPUT_UNIT 64
 
+
+////////////////////////////////////////////////////////////////////////////////
+// UTILITIES to ease wrapping and interfacing with V8
+////////////////////////////////////////////////////////////////////////////////
+
 // Credit: @samcday
 // http://sambro.is-super-awesome.com/2011/03/03/creating-a-proper-buffer-in-a-node-c-addon/ 
 #define MAKE_FAST_BUFFER(NG_SLOW_BUFFER, NG_FAST_BUFFER)      \
@@ -30,198 +35,45 @@ using namespace mkd;
                                                               \
   NG_FAST_BUFFER = NG_JS_BUFFER->NewInstance(3, NG_JS_ARGS);
 
-struct request {
-  Persistent<Function> callback;
-  string in;
-  buf *out; //Pass the output buffer directly
-  unsigned int flags;
-  Persistent<Object> renderer;
-};
+// V8 exception wrapping
 
-////////////////////////
-// Renderer functions //
-////////////////////////
-
-class Renderer : public ObjectWrap {
-protected:
-  mkd_renderer* const ptr;
-public:
-  inline Renderer() : ptr(new mkd_renderer) {}
-  inline mkd_renderer* getHandle() {return ptr;}
-  inline ~Renderer() {
-    delete ptr;
-  }
-  static Handle<Value> newInstance(const Arguments& args);
-};
-
-class HtmlRenderer : public Renderer {
-public:
-  inline HtmlRenderer(unsigned int flags) {
-    upshtml_renderer(ptr, flags);
-  }
-  inline ~HtmlRenderer() {
-    upshtml_free_renderer(ptr);
-  }
-  static Handle<Value> newInstance(const Arguments& args);
-};
-
-Handle<Value> Renderer::newInstance(const Arguments& args) {
-  HandleScope scope;
-
-  (new Renderer)->Wrap(args.This());
-  return scope.Close(args.This());
-}
-
-Handle<Value> HtmlRenderer::newInstance(const Arguments& args) {
-  HandleScope scope;
-
-  unsigned int flags = 0;
-  if (args.Length() >= 1) flags = args[0]->Uint32Value(); //FIXME: introduce method, allow list of flags
-
-  (new HtmlRenderer(flags))->Wrap(args.This());
-  return scope.Close(args.This());
-}
-
-
-//////////////////////
-// Render functions //
-//////////////////////
-
-static void markdownProcess(eio_req *req);
-static int markdownAfter(eio_req *req);
-
-static void outputBufferFree(char* data, void* hint) {
-  buf* b = static_cast<buf*>(hint);
-  bufrelease(b);
-}
-
-static Handle<Value> markdown(const Arguments& args) {
-  HandleScope scope;
+#define V8_WRAP_START()                                                        \
+  HandleScope scope;                                                           \
   try {
-    //Check arguments
-    if (args.Length() < 3 || (!args[0]->IsObject()) || (!args[2]->IsFunction()))
-      throw Exception::TypeError(String::New("usage: markdown(renderer, input, callback, [extensions])"));
 
-    //Extract arguments
-    Local<Object> rend = args[0]->ToObject();
-    String::Utf8Value in(args[1]);
-    Local<Function> callback = Local<Function>::Cast(args[2]);
-    unsigned int flags = 0;
-    if (args.Length() >= 4) flags = args[3]->Uint32Value(); //FIXME
-
-    //Make request
-    request *sr = new request;
-    sr->callback = Persistent<Function>::New(callback);
-    sr->in = *in;
-    sr->flags = flags;
-    sr->renderer = Persistent<Object>::New(rend);
-
-    //Call process
-    eio_custom(markdownProcess, EIO_PRI_DEFAULT, markdownAfter, sr);
-    ev_ref(EV_DEFAULT_UC);
-
-    return scope.Close( Undefined() );
-  } catch (Handle<Value> h) {
-    return ThrowException(h);
-  } catch (exception e) {
-    return ThrowException(Exception::Error(String::New(e.what())));
-  }
-}
-
-static void markdownProcess(eio_req *req) {
-  //Extract request and renderer
-  request *sr = static_cast<request *>(req->data);
-  Renderer* rwrap = Renderer::Unwrap<Renderer>(sr->renderer);
-  mkd_renderer* rend = rwrap->getHandle();
-
-  // Create input buffer from string
-  struct buf *input_buf = bufnew(sr->in.size());
-//  try {FIXME: RAII
-    bufput(input_buf, sr->in.c_str(), sr->in.size());
-
-    // Create output buffer and reset size to 0
-    sr->out = bufnew(OUTPUT_UNIT);
-    sr->out->size = 0;
-
-    // Render!
-    ups_markdown(sr->out, input_buf, rend, sr->flags);
-//  } finally {
-    //Free input buffer
-    bufrelease(input_buf);
-//  }
-}
-
-static int markdownAfter(eio_req *req) {
-  HandleScope scope;
-
-  ev_unref(EV_DEFAULT_UC);
-  request *sr = static_cast<request*>(req->data);
-  Local<Value> argv[1];
-
-  // Create and assign fast buffer to arguments array
-  Buffer* buffer = Buffer::New(const_cast<char *>(sr->out->data), sr->out->size, outputBufferFree, sr->out);
-  Local<Object> fastBuffer;
-  MAKE_FAST_BUFFER(buffer, fastBuffer);
-  argv[0] = fastBuffer;
-
-  // Invoke callback function with html argument
-  TryCatch try_catch;
-  sr->callback->Call(Context::GetCurrent()->Global(), 1, argv);
-  if (try_catch.HasCaught()) {
-    FatalException(try_catch);
+#define V8_WRAP_END()                                                          \
+  } catch (Handle<Value> err) {                                                \
+    return ThrowException(err);                                                \
+  } catch (exception err) {                                                    \
+    return ThrowException(Exception::Error(String::New(err.what())));          \
   }
 
-  /* cleanup */
-  sr->callback.Dispose(); //FIXME: should we put this into a finally (RAII)?
-  return 0;
-}
-
-static Handle<Value> markdownSync(const Arguments &args) {
-  HandleScope scope;
-  try {
-    //Check arguments
-    if (args.Length() < 2 || (!args[0]->IsObject()))
-      throw Exception::TypeError(String::New("usage: markdown(renderer, input, [extensions])"));
-
-    //Extract arguments
-    Renderer* rwrap = Renderer::Unwrap<Renderer>(args[0]->ToObject());
-    mkd_renderer* rend = rwrap->getHandle();
-    String::Utf8Value instr (args[1]);
-    unsigned int flags = 0;
-    if (args.Length() >= 3) flags = args[2]->Uint32Value(); //FIXME
-
-    // Create input buffer from string
-    struct buf *input_buf = bufnew(instr.length());
-  //  try {FIXME: RAII
-      bufput(input_buf, *instr, instr.length());
-
-      // Create output buffer and reset size to 0
-      struct buf *out = bufnew(OUTPUT_UNIT);
-      out->size = 0;
-
-      // Render!
-      ups_markdown(out, input_buf, rend, flags);
-      
-      // Create and return fast buffer to result
-      Buffer* buffer = Buffer::New(const_cast<char *>(out->data), out->size, outputBufferFree, out);
-      Local<Object> fastBuffer;
-      MAKE_FAST_BUFFER(buffer, fastBuffer);
-  //  } finally {
-      //Free input buffer
-      bufrelease(input_buf);
-  //  }
-      return scope.Close(fastBuffer);
-  } catch (Handle<Value> h) {
-    return ThrowException(h);
-  } catch (exception e) {
-    return ThrowException(Exception::Error(String::New(e.what())));
+#define V8_WRAP_END_NR()                                                       \
+  } catch (Handle<Value> err) {                                                \
+    ThrowException(err);                                                       \
+  } catch (exception err) {                                                    \
+    ThrowException(Exception::Error(String::New(err.what())));                 \
   }
+
+// Dealing with V8 persistent handles
+
+template <class T> void ClearPersistent(Persistent<T>& handle) {
+    if (handle.IsEmpty()) return;
+    handle.Dispose();
+    handle.Clear();
+}
+
+template <class T> void SetPersistent(Persistent<T>& handle, Handle<T> value) {
+    ClearPersistent<T>(handle);
+    if (value.IsEmpty()) return;
+    handle = Persistent<T>::New(value);
 }
 
 
-////////////////////////
-// Module declaration //
-////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// MODULE DECLARATION
+////////////////////////////////////////////////////////////////////////////////
 
 extern "C" {
   void init (Handle<Object> target) {
@@ -242,23 +94,8 @@ extern "C" {
     target->Set(String::NewSymbol("EXT_TABLES"), Integer::New(MKDEXT_TABLES));
 
     //TODO: html renderer flags
-
-    //Renderer class
-    Local<FunctionTemplate> rendL = FunctionTemplate::New(&Renderer::newInstance);
-    Persistent<FunctionTemplate> rend = Persistent<FunctionTemplate>::New(rendL);
-    rend->InstanceTemplate()->SetInternalFieldCount(1);
-    rend->SetClassName(String::NewSymbol("Renderer"));
-
-    target->Set(String::NewSymbol("Renderer"), rend->GetFunction());
-
-    //Standard HTML renderer class
-    Local<FunctionTemplate> htmlrendL = FunctionTemplate::New(&HtmlRenderer::newInstance);
-    Persistent<FunctionTemplate> htmlrend = Persistent<FunctionTemplate>::New(htmlrendL);
-    htmlrend->InstanceTemplate()->SetInternalFieldCount(1);
-    htmlrend->Inherit(rend);
-    htmlrend->SetClassName(String::NewSymbol("HtmlRenderer"));
-
-    target->Set(String::NewSymbol("HtmlRenderer"), htmlrend->GetFunction());
+    
+    //TODO
   }
   NODE_MODULE(robotskirt, init)
 }
