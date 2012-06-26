1901fd3abb95055b2156460bc4e4e84f92e02f9b
Xavier Mendez <jmendeth@gmail.com>
2012-06-26 20:03:08 +0200

    More [temporal] cleanup

--- src/markdownWrapper.hpp
+++ src/markdownWrapper.hpp
@@ -8,26 +8,26 @@ extern "C" {
 
 namespace mkd {
 
-    class Renderer {
-    protected:
-        mkd_renderer ptr;
-    public:
-        inline Renderer() {}
-        inline ~Renderer() {}
-        inline mkd_renderer& getRenderer() {return ptr;}
-    };
-
-    class HtmlRenderer: public Renderer {
-    public:
-        inline HtmlRenderer(unsigned int render_flags) {upshtml_renderer(&ptr, render_flags);}
-        inline ~HtmlRenderer() {upshtml_free_renderer(&ptr);}
-    };
-
-    class TocHtmlRenderer: public Renderer {
-    public:
-        inline TocHtmlRenderer() {upshtml_toc_renderer(&ptr);}
-        inline ~TocHtmlRenderer() {upshtml_free_renderer(&ptr);}
-    };
+//    class Renderer {
+//    protected:
+//        mkd_renderer ptr;
+//    public:
+//        inline Renderer() {}
+//        inline ~Renderer() {}
+//        inline mkd_renderer& getRenderer() {return ptr;}
+//    };
+//
+//    class HtmlRenderer: public Renderer {
+//    public:
+//        inline HtmlRenderer(unsigned int render_flags) {upshtml_renderer(&ptr, render_flags);}
+//        inline ~HtmlRenderer() {upshtml_free_renderer(&ptr);}
+//    };
+//
+//    class TocHtmlRenderer: public Renderer {
+//    public:
+//        inline TocHtmlRenderer() {upshtml_toc_renderer(&ptr);}
+//        inline ~TocHtmlRenderer() {upshtml_free_renderer(&ptr);}
+//    };
 
 };
 
