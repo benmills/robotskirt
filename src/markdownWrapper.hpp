8131dccfa5aed71e1a19541a33e492bbfaf5e01f
Xavier Mendez <jmendeth@gmail.com>
2012-07-26 17:55:17 +0200

    Add BufWrap in markdownWrapper.hpp

--- src/markdownWrapper.hpp
+++ src/markdownWrapper.hpp
@@ -8,26 +8,18 @@ extern "C" {
 
 namespace mkd {
 
-//    class Renderer {
-//    protected:
-//        mkd_renderer ptr;
-//    public:
-//        inline Renderer() {}
-//        inline ~Renderer() {}
-//        inline mkd_renderer& getRenderer() {return ptr;}
-//    };
-//
-//    class HtmlRenderer: public Renderer {
-//    public:
-//        inline HtmlRenderer(unsigned int render_flags) {upshtml_renderer(&ptr, render_flags);}
-//        inline ~HtmlRenderer() {upshtml_free_renderer(&ptr);}
-//    };
-//
-//    class TocHtmlRenderer: public Renderer {
-//    public:
-//        inline TocHtmlRenderer() {upshtml_toc_renderer(&ptr);}
-//        inline ~TocHtmlRenderer() {upshtml_free_renderer(&ptr);}
-//    };
+    class BufWrap {
+    public:
+        BufWrap(buf* buf): buf_(buf) {}
+        ~BufWrap() {
+            bufrelease(buf_);
+        }
+        buf* get() {return buf_;}
+        buf* operator->() {return buf_;}
+        buf* operator*() {return buf_;}
+    private:
+        buf* const buf_;
+    };
 
 };
 
