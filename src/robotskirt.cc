83c42b4284aee1448c259dd5606e3479501d5a92
Xavier Mendez <jmendeth@gmail.com>
2012-08-25 11:37:57 +0200

    Bump version to 2.3

--- src/robotskirt.cc
+++ src/robotskirt.cc
@@ -932,7 +932,7 @@ NODE_DEF_MAIN() {
     initVersion(target);
     Local<Object> versions = Obj();
     versions->Set(Symbol("sundown"), SundownVersion());
-    versions->Set(Symbol("robotskirt"), (new Version(2,2,3))->Wrapped());
+    versions->Set(Symbol("robotskirt"), (new Version(2,3,0))->Wrapped());
     target->Set(Symbol("versions"), versions);
 
     //Extension constants
