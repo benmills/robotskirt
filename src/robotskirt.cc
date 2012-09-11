9f532153280e6c1a9c6f1b0200145b01d4bc07e0
Xavier Mendez <jmendeth@gmail.com>
2012-09-11 20:53:46 +0200

    Bump minor version to 2.5.0

--- src/robotskirt.cc
+++ src/robotskirt.cc
@@ -1069,7 +1069,7 @@ NODE_DEF_MAIN() {
     Version::init(target);
     Local<Object> versions = Obj();
     versions->Set(Symbol("sundown"), SundownVersion());
-    versions->Set(Symbol("robotskirt"), (new Version(2,4,0))->Wrapped());
+    versions->Set(Symbol("robotskirt"), (new Version(2,5,0))->Wrapped());
     target->Set(Symbol("versions"), versions);
 
     //Extension constants
