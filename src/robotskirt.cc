1df0989526243321b5fb8639dbc526861e072aa4
Xavier Mendez <jmendeth@gmail.com>
2012-09-15 11:24:15 +0200

    Bump patch version to 2.5.2

--- src/robotskirt.cc
+++ src/robotskirt.cc
@@ -1075,7 +1075,7 @@ NODE_DEF_MAIN() {
     Version::init(target);
     Local<Object> versions = Obj();
     versions->Set(Symbol("sundown"), SundownVersion());
-    versions->Set(Symbol("robotskirt"), (new Version(2,5,1))->Wrapped());
+    versions->Set(Symbol("robotskirt"), (new Version(2,5,2))->Wrapped());
     target->Set(Symbol("versions"), versions);
 
     //Extension constants
